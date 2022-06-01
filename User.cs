54c2bebffe5da06a17dbf73a58905b98c0baf93d
caemanswartz <caemanswartz@gmail.com>
2022-06-01 12:13:30 -0700

    delete extra file

--- User.cs
+++
@@ -1,9 +0,0 @@
-namespace QuHwJwtAspNetCoreWebApi
-{
-    public class User
-    {
-        public string Username{get;set;}=string.Empty;
-        public byte[] PasswordHash{get;set;}
-        public byte[] PasswordSalt{get;set;}
-    }
-}
